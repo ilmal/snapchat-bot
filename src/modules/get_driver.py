f59142fd1b1003daa761d20525bcb8076ae2b6f1
ilmal <nils@u1.se>
2023-09-05 08:49:01 +0200

    adding stuff

--- src/modules/get_driver.py
+++ src/modules/get_driver.py
@@ -18,9 +18,9 @@ def get_driver():
     driver_user_path = dirname.replace("modules", "driver_user")
 
     options = webdriver.ChromeOptions()
-    #options.add_argument("--window-size=1920,1080")
-    #options.add_argument("--headless=new")
-    #options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
     options.add_argument('--start-maximized')
     options.add_argument('--no-sandbox')
     #options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
